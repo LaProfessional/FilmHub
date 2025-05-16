9546d743808eec1cb6356f06654b9cc58a3ba2eb
pavel <pavelrazrushitel1ads@mail.ru>
2025-05-17 01:37:33 +0700

    wip

--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -1,9 +1,30 @@
+import styles from "../../../shared/styles/layout/Header.module.scss";
+
+import { ReactComponent as LogoSvg } from "../../../shared/assets/header/Logo.svg";
+// import { ReactComponent as SearchSvg } from "../../../shared/assets/header/Search.svg";
+import { ReactComponent as SunSvg } from "../../../shared/assets/header/Sun.svg";
+// import { ReactComponent as MoonSvg } from "../../../shared/assets/header/Moon.svg";
 
 const Header = () => {
     return (
-        <div>
+        <header className={ styles.headerContainer }>
+            <div className={ styles.header }>
+                <div className={ styles.headerTitle }>
+                    <LogoSvg className={ styles.logoSvg }/>
+                    <h2 className={ styles.title }>FilmHub</h2>
+                </div>
 
-        </div>
+                <nav>
+                    <input
+                        className={ styles.input }
+                        type="text"
+                        placeholder="Поиск фильмов..."
+                    />
+                    <SunSvg/>
+                    <img src="../../../shared/assets/header/ProfilePhoto.jpg" alt="ProfilePhoto"/>
+                </nav>
+            </div>
+        </header>
     );
 };
 
