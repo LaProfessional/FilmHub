10703c61e3e2c598f0d6dc041b6578c6d6fa6619
pavel <pavelrazrushitel1ads@mail.ru>
2025-05-17 01:37:11 +0700

    wip

---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,12 @@
+import "../shared/styles/base/reset.scss";
+
+import Header from "../widgets/header/ui/Header.tsx";
+
+const App = () => {
+
+    return (
+        <Header></Header>
+    );
+};
+
+export default App;
---
+++ frontend/src/shared/types/declaration.d.ts
@@ -0,0 +1,9 @@
+declare module "*module.scss" {
+    const styles: { [key: string]: string };
+    export default styles;
+}
+
+declare module "*.svg" {
+    const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    export { ReactComponent };
+}
