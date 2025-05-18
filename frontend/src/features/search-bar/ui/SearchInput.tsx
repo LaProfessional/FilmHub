42631ffb77e4de60b23b919c2e72ab565c447275
pavel <pavelrazrushitel1ads@mail.ru>
2025-05-18 21:09:42 +0700

    wip

---
+++ frontend/src/features/search-bar/ui/SearchInput.tsx
@@ -0,0 +1,19 @@
+import styles from "./SearchInput.module.scss";
+
+import { ReactComponent as SearchSvg } from "../../../shared/assets/header/Search.svg";
+
+
+const SearchInput = () => {
+    return (
+        <div className={ styles.wrapper }>
+            <input
+                className={ styles.input }
+                type="text"
+                placeholder="Поиск фильмов..."
+            />
+            <SearchSvg className={ styles.searchSvg }></SearchSvg>
+        </div>
+    );
+};
+
+export default SearchInput;
--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -1,9 +1,10 @@
-import styles from "../../../shared/styles/layout/Header.module.scss";
+import styles from "./Header.module.scss";
 
 import { ReactComponent as LogoSvg } from "../../../shared/assets/header/Logo.svg";
-// import { ReactComponent as SearchSvg } from "../../../shared/assets/header/Search.svg";
 import { ReactComponent as SunSvg } from "../../../shared/assets/header/Sun.svg";
 // import { ReactComponent as MoonSvg } from "../../../shared/assets/header/Moon.svg";
+
+import SearchInput from "../../../features/search-bar/ui/SearchInput.tsx";
 
 const Header = () => {
     return (
@@ -14,14 +15,11 @@ const Header = () => {
                     <h2 className={ styles.title }>FilmHub</h2>
                 </div>
 
-                <nav>
-                    <input
-                        className={ styles.input }
-                        type="text"
-                        placeholder="Поиск фильмов..."
-                    />
-                    <SunSvg/>
-                    <img src="../../../shared/assets/header/ProfilePhoto.jpg" alt="ProfilePhoto"/>
+                <nav className={ styles.nav }>
+                    <SearchInput></SearchInput>
+                    <SunSvg className={ styles.sunSvg }/>
+                    {/*<img src="../../../shared/assets/header/ProfilePhoto.jpg" alt="ProfilePhoto"/>*/}
+
                 </nav>
             </div>
         </header>
