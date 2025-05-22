36afea1232ecb34890b43403c699144b2423a420
pavel <pavelrazrushitel1ads@mail.ru>
2025-05-22 23:44:27 +0700

    wip

--- frontend/src/widgets/sidebar/ui/Sidebar.tsx
+++ frontend/src/widgets/sidebar/ui/Sidebar.tsx
@@ -2,7 +2,6 @@ import styles from "./Sidebar.module.scss";
 import { useTranslation } from "react-i18next";
 
 import { ReactComponent as ArrowSvg } from "../../../shared/assets/sidebar/Arrow.svg";
-import { ReactComponent as ExpandSvg } from "../../../shared/assets/sidebar/Expand.svg";
 
 import AddCategoryInput from "../../../features/sidebar/ui/AddCategoryInput.tsx";
 import AddBtn from "../../../features/sidebar/model/AddBtn.tsx";
@@ -34,29 +33,99 @@ const Sidebar = () => {
 
                 <section>
                     <div className={ styles.wrapper }>
-                        <h2 className={ styles.title }>Категории</h2>
+                        <h2 className={ styles.title }>{ t("categories") }</h2>
                         <button className={ styles.collapseExpandBtn }>
                             <ArrowSvg className={ styles.arrowSvg }/>
                         </button>
                     </div>
 
                     <div className={ styles.categoryGroup }>
-                        <h3 className={ styles.categoryGroupTitle }>Уровень сюжета</h3>
-                        <button className={ styles.collapseExpandCategoryBtn }>
-                            <ExpandSvg className={ styles.expandSvg }/>
+                        <h3 className={ styles.categoryGroupTitle }>{ t("storyLevel") }</h3>
+                        <button>
+                            <ArrowSvg className={ styles.arrowSvg }/>
                         </button>
                     </div>
 
-                    <ul>
-                        <li className={ styles.flag }>Нарративный хаос</li>
-                        <li className={ styles.flag }>Предсказуемая обыденность</li>
-                        <li className={ styles.flag }>Удовлетворительная структура</li>
-                        <li className={ styles.flag }>Захватывающее повествование</li>
-                        <li className={ styles.flag }>Литературный шедевр</li>
+                    <ul className={ styles.flagContainer }>
+                        <li
+                            className={ styles.flag }
+                            title={ t("narrativeChaos") }>{ t("narrativeChaos") }
+                        </li>
+                        <li
+                            className={ styles.flag }
+                            title={ t("predictableOrdinariness") }>{ t("predictableOrdinariness") }
+                        </li>
+                        <li
+                            className={ styles.flag }
+                            title={ t("satisfactoryStructure") }>{ t("satisfactoryStructure") }
+                        </li>
+                        <li
+                            className={ styles.flag }
+                            title={ t("grippingNarrative") }>{ t("grippingNarrative") }
+                        </li>
+                        <li
+                            className={ styles.flag }
+                            title={ t("literaryMasterpiece") }>{ t("literaryMasterpiece") }
+                        </li>
                     </ul>
 
-                    <AddBtn>Добавить флаг</AddBtn>
+                    <AddBtn>{ t("addFlag") }</AddBtn>
                 </section>
+
+                {/*<section>*/}
+                {/*    <div className={ styles.wrapper }>*/}
+                {/*        <h2 className={ styles.title }>Статистика флагов</h2>*/}
+                {/*        <button className={ styles.collapseExpandBtn }>*/}
+                {/*            <ArrowSvg className={ styles.arrowSvg }/>*/}
+                {/*        </button>*/}
+                {/*    </div>*/}
+
+
+                {/*    <div className={ styles.categoryGroup }>*/}
+                {/*        <h3 className={ styles.categoryGroupTitle }>{ t("storyLevel") }</h3>*/}
+                {/*        <button>*/}
+                {/*            <ArrowSvg className={ styles.arrowSvg }/>*/}
+                {/*        </button>*/}
+                {/*    </div>*/}
+
+                {/*    <ul className={ styles.flagContainer }>*/}
+                {/*        <li className={ styles.flagData }>*/}
+                {/*            <div className={ styles.wrapperFlagStat }>*/}
+                {/*                <span*/}
+                {/*                    className={ `${ styles.flag } ${ styles.flagStat }` }*/}
+                {/*                    title={ t("narrativeChaos") }>{ t("narrativeChaos") }*/}
+                {/*                </span>*/}
+
+                {/*                <div className={ styles.amountFlag }>3</div>*/}
+                {/*            </div>*/}
+
+                {/*            <div className={ styles.progressBar }>*/}
+                {/*                <div className={ styles.progress }></div>*/}
+                {/*            </div>*/}
+                {/*        </li>*/}
+
+                {/*        <li*/}
+                {/*            className={ `${ styles.flag } ${ styles.flagStat }` }*/}
+                {/*            title={ t("predictableOrdinariness") }>{ t("predictableOrdinariness") }*/}
+                {/*        </li>*/}
+
+                {/*        <li*/}
+                {/*            className={ `${ styles.flag } ${ styles.flagStat }` }*/}
+                {/*            title={ t("satisfactoryStructure") }>{ t("satisfactoryStructure") }*/}
+                {/*        </li>*/}
+
+                {/*        <li*/}
+                {/*            className={ `${ styles.flag } ${ styles.flagStat }` }*/}
+                {/*            title={ t("grippingNarrative") }>{ t("grippingNarrative") }*/}
+                {/*        </li>*/}
+
+                {/*        <li*/}
+                {/*            className={ `${ styles.flag } ${ styles.flagStat }` }*/}
+                {/*            title={ t("literaryMasterpiece") }>{ t("literaryMasterpiece") }*/}
+                {/*        </li>*/}
+                {/*    </ul>*/}
+
+                {/*</section>*/}
             </nav>
         </aside>
     );
