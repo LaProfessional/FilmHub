30503c60b5c7bba7d8a834aa1f27b0217fdb3655
VladimirBerl <vova2002.13lol@gmail.com>
2025-05-24 17:28:49 +0300

    global refactoring code & add router

---
+++ frontend/src/index.tsx
@@ -0,0 +1,16 @@
+import App from "./app/App.tsx";
+import { createRoot } from "react-dom/client";
+
+import { ThemeProvider } from "./app/providers/theme";
+import { LanguageProvider } from "./app/providers/i18n/";
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <LanguageProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </LanguageProvider>
+  </BrowserRouter>
+);
