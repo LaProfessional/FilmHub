28f2a98bfb8f72f12897b8623ab59a4bc2a62f69
pavel <pavelrazrushitel1ads@mail.ru>
2025-05-30 21:19:43 +0700

    fix rem

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,31 +5,6 @@ import {ThemeProvider} from "./app/providers/theme";
 import {LanguageProvider} from "./app/providers/i18n/";
 import {BrowserRouter} from "react-router-dom";
 
-const data = JSON.stringify({
-    email: 'test@test-a.com',
-    password: '11111111',
-})
-
-const resp = await fetch("http://localhost:4444/auth/login", {
-    method: 'POST',
-    body: data,
-    headers: {
-        "Content-Type": "application/json",
-    }
-})
-
-const resp1 = await resp.json()
-console.log(resp1)
-
-const respS = await fetch("http://localhost:4444/auth/security", {
-    method: 'GET',
-    headers: {
-        "Authorization": `Bearer ${resp1.access}`
-    }
-})
-
-console.log(respS)
-
 createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
         <LanguageProvider>
