8d3d8bb838feaf59dc9a237d5ed25188110e3cb3
boyra <b9892213@gmail.com>
2025-06-11 17:53:57 +0300

    refactoring remove unnecessary space

--- frontend/src/app/providers/router/ui/AppRouter.tsx
+++ frontend/src/app/providers/router/ui/AppRouter.tsx
@@ -3,11 +3,7 @@ import { routeConfig } from "@/shared/config/router";
 import { Suspense } from "react";
 
 export const AppRouter = () => {
-    const routes = useRoutes(routeConfig);
+  const routes = useRoutes(routeConfig);
 
-    return (
-        <Suspense fallback={ <div>Loading...</div> }>
-            { routes }
-        </Suspense>
-    );
+  return <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>;
 };
