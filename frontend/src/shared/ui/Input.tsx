b97ebe72f9da16392150820d877966b304c98851
boyra <b9892213@gmail.com>
2025-06-11 16:35:27 +0300

    refactoring remove unnecessary space

--- frontend/src/shared/ui/Input.tsx
+++ frontend/src/shared/ui/Input.tsx
@@ -2,15 +2,12 @@ import styles from "./Input.module.scss";
 import cls from "@fvilers/cls";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-    variant: string;
-    error?: string;
+  variant: string;
+  error?: string;
 }
 
 export const Input: React.FC<InputProps> = ({ variant, error, ...props }) => {
-    return (
-        <input
-            className={ cls(styles[variant], error && styles.error) }
-            { ...props }
-        />
-    );
+  return (
+    <input className={cls(styles[variant], error && styles.error)} {...props} />
+  );
 };
--- frontend/src/widgets/header/ui/Header.tsx
+++ frontend/src/widgets/header/ui/Header.tsx
@@ -1,7 +1,7 @@
 import styles from "./Header.module.scss";
 import { UserMenu } from "./UserMenu/UserMenu";
 import { Button } from "@/shared/ui/Button";
-import titleStyles from "@/shared/styles/components/TitleStyles.module.scss"
+import titleStyles from "@/shared/styles/components/TitleStyles.module.scss";
 
 import { ReactComponent as LogoSvg } from "@/shared/assets/header/Logo.svg";
 import { ReactComponent as SunSvg } from "@/shared/assets/header/Sun.svg";
@@ -15,41 +15,48 @@ import { useLanguage } from "@/app/providers/i18n/ui/LanguageProvider";
 import { useTranslation } from "react-i18next";
 
 export const Header = () => {
-    const { theme, toggleTheme } = useTheme();
-    const { language, changeLanguage } = useLanguage();
+  const { theme, toggleTheme } = useTheme();
+  const { language, changeLanguage } = useLanguage();
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <header className={ styles.header }>
-            <div className={ styles.titleWrapper }>
-                <LogoSvg className={ styles.logoSvg }/>
-                <h2 className={ titleStyles.titleGradient }>FilmHub</h2>
-            </div>
+  return (
+    <header className={styles.header}>
+      <div className={styles.titleWrapper}>
+        <LogoSvg className={styles.logoSvg} />
+        <h2 className={titleStyles.titleGradient}>FilmHub</h2>
+      </div>
 
-            <nav className={ styles.nav }>
-                <div className={ styles.wrapper }>
-                    <Input
-                        variant={ "inputSearch" }
-                        type="text"
-                        placeholder={ t("SearchMovies") }
-                    />
+      <nav className={styles.nav}>
+        <div className={styles.wrapper}>
+          <Input
+            variant={"inputSearch"}
+            type="text"
+            placeholder={t("SearchMovies")}
+          />
 
-                    <SearchSvg className={ styles.searchSvg }></SearchSvg>
-                </div>
+          <SearchSvg className={styles.searchSvg}></SearchSvg>
+        </div>
 
+        <Button variant="headerButton" onClick={toggleTheme}>
+          {theme === "dark" ? (
+            <SunSvg className={styles.sunSvg} />
+          ) : (
+            <MoonSvg />
+          )}
+        </Button>
 
-                <Button variant="headerButton" onClick={ toggleTheme }>
-                    { theme === "dark" ? <SunSvg className={ styles.sunSvg }/> : <MoonSvg/> }
-                </Button>
+        <Button
+          variant="headerButton"
+          onClick={() => changeLanguage(language === "en" ? "ru" : "en")}
+        >
+          <span className={styles.language}>
+            {language === "en" ? "en" : "ru"}
+          </span>
+        </Button>
 
-                <Button variant="headerButton" onClick={ () => changeLanguage(language === "en" ? "ru" : "en") }>
-                    <span className={ styles.language }>{ language === "en" ? "en" : "ru" }</span>
-                </Button>
-
-
-                <UserMenu/>
-            </nav>
-        </header>
-    );
+        <UserMenu />
+      </nav>
+    </header>
+  );
 };
