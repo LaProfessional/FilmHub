e773664f8fb25dbb296269112f576d092878743f
boyra <b9892213@gmail.com>
2025-06-13 16:34:19 +0300

    refactoring remove old react-router-dom

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 
 import { ThemeProvider } from "./app/providers/theme";
 import { LanguageProvider } from "./app/providers/i18n/";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <LanguageProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </LanguageProvider>
-  </BrowserRouter>
+  <LanguageProvider>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </LanguageProvider>
 );
