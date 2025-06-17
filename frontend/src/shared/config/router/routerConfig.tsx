6e624fbed8c4807204328524c3032df60cd43e51
boyra <b9892213@gmail.com>
2025-06-17 14:35:30 +0300

    remove unnecessary space and import homeLoader

--- frontend/src/shared/config/router/routerConfig.tsx
+++ frontend/src/shared/config/router/routerConfig.tsx
@@ -1,24 +1,26 @@
+import homeLoader from "@/app/route-loaders/homeLoader";
 import { HomePage } from "@/pages/home";
 import { RootLayout } from "@/shared/layout/ui/RootLayout";
 import { Navigate, type RouteObject } from "react-router-dom";
 
 export const routeConfig: RouteObject[] = [
-    {
-        path: "/",
-        element: <Navigate to="/home" replace />,
-    },
-    // {
-    //     path: "/auth",
-    //     element: <LoginModal />,
-    // },
-    {
-        path: "/home",
-        element: <RootLayout />,
-        children: [
-            {
-                index: true,
-                element: <HomePage />,
-            },
-        ],
-    },
+  {
+    path: "/",
+    element: <Navigate to="/home" replace />,
+  },
+  // {
+  //     path: "/auth",
+  //     element: <LoginModal />,
+  // },
+  {
+    path: "/home",
+    element: <RootLayout />,
+    loader: homeLoader,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
 ];
