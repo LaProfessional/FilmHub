d1aa50289c04574bee4f694fcc0918706becc7f8
boyra <b9892213@gmail.com>
2025-06-17 14:30:38 +0300

    add "type" prop to "InputProps" interface and "name" attribute to input field

--- frontend/src/shared/ui/Input.tsx
+++ frontend/src/shared/ui/Input.tsx
@@ -5,20 +5,29 @@ import { useState } from "react";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   variant: string;
   error?: string;
+  type?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ variant, error, ...props }) => {
+export const Input: React.FC<InputProps> = ({
+  variant,
+  error,
+  type,
+  ...props
+}) => {
   const [text, setText] = useState<string>("");
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     const typedText = e.target.value;
+    console.log("input type from shared/ui/input");
     setText(typedText);
   }
   return (
     <input
       value={text}
+      type={type}
       onChange={handleChange}
       className={cls(styles[variant], error && styles.error)}
+      name="search"
       {...props}
     />
   );
