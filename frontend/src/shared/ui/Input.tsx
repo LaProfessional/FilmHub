076c4802e97dcae16648c9263fb70da5d957a640
boyra <b9892213@gmail.com>
2025-06-26 14:16:24 +0300

    refactoring remove "type" from interface and "useState" hook

--- frontend/src/shared/ui/Input.tsx
+++ frontend/src/shared/ui/Input.tsx
@@ -5,27 +5,11 @@ import { useState } from "react";
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   variant: string;
   error?: string;
-  type?: string;
 }
 
-export const Input: React.FC<InputProps> = ({
-  variant,
-  error,
-  type,
-  ...props
-}) => {
-  const [text, setText] = useState<string>("");
-
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const typedText = e.target.value;
-    console.log("input type from shared/ui/input");
-    setText(typedText);
-  }
+export const Input: React.FC<InputProps> = ({ variant, error, ...props }) => {
   return (
     <input
-      value={text}
-      type={type}
-      onChange={handleChange}
       className={cls(styles[variant], error && styles.error)}
       name="search"
       {...props}
