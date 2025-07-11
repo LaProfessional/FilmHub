c3a7c012213a70928340f24503ee15f9dbec80cb
Rummiya <rumibashirova@gmail.com>
2025-07-11 19:38:07 +0600

    feat(user-settings): обернуто окно с настройками в shadcn dialog

--- frontend/src/widgets/user-settings/ui/UserSettings.tsx
+++ frontend/src/widgets/user-settings/ui/UserSettings.tsx
@@ -1,19 +1,41 @@
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogOverlay,
+  DialogPortal,
+  DialogTrigger,
+} from "@/shared/ui";
+import { Settings } from "lucide-react";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import { getSettingsData } from "../model/getSettingsData";
 import type { NavItemKey } from "../model/types";
 import { SettingsOptions } from "./SettingsOptions";
 import { SettingsSidebar } from "./SettingsSidebar";
 
 export const UserSettings = () => {
-  const [activeTab, setActiveTab] = useState<NavItemKey>("gallery");
+  const { t } = useTranslation();
   const { settings } = getSettingsData();
+  const [activeTab, setActiveTab] = useState<NavItemKey>("gallery");
 
   return (
-    <div className="bg-background text-foreground w-full h-screen fixed top-0 left-0">
-      <div className="h-full flex">
-        <SettingsSidebar setActiveTab={setActiveTab} activeTab={activeTab} />
-        <SettingsOptions settings={settings[activeTab]} />
-      </div>
-    </div>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>
+          <Settings size={18} />
+          <span>{t("Settings")}</span>
+        </Button>
+      </DialogTrigger>
+
+      <DialogPortal>
+        <DialogOverlay className="fixed inset-0 z-50 bg-black/60" />
+
+        <DialogContent className="min-w-screen h-screen rounded-none flex p-0">
+          <SettingsSidebar setActiveTab={setActiveTab} activeTab={activeTab} />
+          <SettingsOptions settings={settings[activeTab]} />
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
   );
 };
