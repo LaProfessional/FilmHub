48e3040edb26a9eaa45b3a1fc421d8e8cfe580f6
Rummiya <rumibashirova@gmail.com>
2025-07-11 20:06:07 +0600

    feat(user-settings): добавлено большое количевство параметров для теста

--- frontend/src/widgets/user-settings/model/getSettingsData.ts
+++ frontend/src/widgets/user-settings/model/getSettingsData.ts
@@ -23,10 +23,66 @@ export const getSettingsData = () => {
       options: [
         {
           value: "Описание под карточкой",
-          type: "checkbox",
+          type: "switch",
+        },
+        {
+          value: "Без отступов по краям",
+          type: "switch",
         },
         {
           value: "Для 4к мониторов",
+          type: "checkbox",
+        },
+        {
+          value: "Слайдер",
+          type: "switch",
+        },
+        {
+          value: "Список",
+          type: "switch",
+        },
+        {
+          value: "Сетка",
+          type: "switch",
+        },
+        {
+          value: "Большие карточки",
+          type: "switch",
+        },
+        {
+          value: "Отключить отображение флагов на фильме",
+          type: "switch",
+        },
+        {
+          value: "Показывать только картинку флага",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
+          type: "switch",
+        },
+        {
+          value: "Маленькие карточки",
           type: "switch",
         },
       ],
@@ -35,12 +91,12 @@ export const getSettingsData = () => {
       title: "Sidebar",
       options: [
         {
-          value: "Описание под карточкой",
+          value: "Показывать progress bar флагов",
           type: "checkbox",
         },
         {
-          value: "Для 4к мониторов",
-          type: "switch",
+          value: "Показывать количество флагов",
+          type: "checkbox",
         },
       ],
     },
