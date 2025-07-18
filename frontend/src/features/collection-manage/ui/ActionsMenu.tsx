39d0e8967fd5a5f9bd984f3ccd28ff6da4de1db2
Rummiya <rumibashirova@gmail.com>
2025-07-18 18:50:44 +0600

    feat(collection-manage): добавлена возможность менять имя коллекции
    -Создан компонент RenameCollection
    -Передан триггер onEdit в ActionMenu
    -Добавлено состояние редактирования для NavItem

--- frontend/src/features/collection-manage/ui/ActionsMenu.tsx
+++ frontend/src/features/collection-manage/ui/ActionsMenu.tsx
@@ -7,14 +7,14 @@ import {
 } from "@/shared/ui";
 import { MoreVerticalIcon } from "lucide-react";
 
-export const ActionsMenu = () => {
+export const ActionsMenu = ({ onEdit }: { onEdit: () => void }) => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="cursor-pointer opacity-0 group-hover:opacity-100 data-[state=open]:opacity-100 transition-all delay-150">
         <MoreVerticalIcon size={18} />
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuItem>Rename</DropdownMenuItem>
+        <DropdownMenuItem onClick={onEdit}>Rename</DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem>Delete</DropdownMenuItem>
       </DropdownMenuContent>
--- frontend/src/widgets/sidebar/ui/NavItem.tsx
+++ frontend/src/widgets/sidebar/ui/NavItem.tsx
@@ -1,13 +1,20 @@
-import type { Collection } from "@/entities/collection/model";
+import { type Collection } from "@/entities/collection/model";
 import { ActionsMenu } from "@/features/collection-manage/ui/ActionsMenu";
+import { RenameCollection } from "@/features/collection-manage/ui/RenameCollection";
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 export const NavItem = ({ collection }: { collection: Collection }) => {
-  return (
-    <li className="group p-2 cursor-pointer text-base flex items-center justify-between transition-all delay-150 hover:opacity-50">
-      <NavLink to="/">{collection.name}</NavLink>
+  const [editing, setEditing] = useState(false);
 
-      <ActionsMenu />
-    </li>
-  );
+  const toggleEditing = () => {
+    setEditing((prev) => !prev);
+  };
+
+  return editing ?
+      <li className="group p-2 cursor-pointer text-base flex items-center justify-between transition-all delay-150 hover:opacity-50">
+        <NavLink to="/">{collection.name}</NavLink>
+        <ActionsMenu onEdit={toggleEditing} />
+      </li>
+    : <RenameCollection col={collection} onSuccess={toggleEditing} />;
 };
