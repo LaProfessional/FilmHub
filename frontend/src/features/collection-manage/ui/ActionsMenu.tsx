d2142463c489bfb3a7a6a0b0569816c6d0cdd7c5
Rummiya <rumibashirova@gmail.com>
2025-07-18 17:42:27 +0600

    feat(collection-manage): создано ActionsMenu на основе DropDownMenu из shadcn, заменен Button на ActionsMenu

---
+++ frontend/src/features/collection-manage/ui/ActionsMenu.tsx
@@ -0,0 +1,23 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/shared/ui";
+import { MoreVerticalIcon } from "lucide-react";
+
+export const ActionsMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="cursor-pointer opacity-0 group-hover:opacity-100 data-[state=open]:opacity-100 transition-all delay-150">
+        <MoreVerticalIcon size={18} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>Rename</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- frontend/src/widgets/sidebar/ui/NavItem.tsx
+++ frontend/src/widgets/sidebar/ui/NavItem.tsx
@@ -1,5 +1,4 @@
-import { Button } from "@/shared/ui";
-import { MoreVerticalIcon } from "lucide-react";
+import { ActionsMenu } from "@/features/collection-manage/ui/ActionsMenu";
 import { NavLink } from "react-router";
 
 export const NavItem = ({ name }: { name: string }) => {
@@ -7,9 +6,7 @@ export const NavItem = ({ name }: { name: string }) => {
     <li className="group p-2 cursor-pointer text-base flex items-center justify-between transition-all delay-150 hover:opacity-50">
       <NavLink to="/">{name}</NavLink>
 
-      <Button className="opacity-0 group-hover:opacity-100 cursor-pointer" variant={"ghost"}>
-        <MoreVerticalIcon size={18} />
-      </Button>
+      <ActionsMenu />
     </li>
   );
 };
