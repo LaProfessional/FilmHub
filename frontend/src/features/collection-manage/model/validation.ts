ec70716275e51f638216ffbb825bed5233566fac
Rummiya <rumibashirova@gmail.com>
2025-07-19 12:31:31 +0600

    refactor(collection-manage): изменено ограничение символов с 12 до 60 в collectionSchema

--- frontend/src/features/collection-manage/model/validation.ts
+++ frontend/src/features/collection-manage/model/validation.ts
@@ -4,7 +4,7 @@ export const collectionSchema = z.object({
   name: z
     .string()
     .min(3, "Введите название папки")
-    .max(12, "Название не должно быть длиннее 12 символов"),
+    .max(60, "Название не должно быть длиннее 60 символов"),
 });
 
 export type CollectionSchema = z.infer<typeof collectionSchema>;
