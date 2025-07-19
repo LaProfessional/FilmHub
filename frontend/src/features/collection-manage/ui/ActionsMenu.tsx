f3dd9e916f2967a3ba379d39566c4d6230de67cd
Rummiya <rumibashirova@gmail.com>
2025-07-19 11:56:24 +0600

    feat(collection-menu): добавлена поддержка перевода в ActionMenu

--- frontend/src/features/collection-manage/ui/ActionsMenu.tsx
+++ frontend/src/features/collection-manage/ui/ActionsMenu.tsx
@@ -6,8 +6,11 @@ import {
   DropdownMenuTrigger,
 } from "@/shared/ui";
 import { Edit2, MoreVerticalIcon, ShareIcon, Trash2 } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 export const ActionsMenu = ({ onDelete, onEdit }: { onDelete: () => void; onEdit: () => void }) => {
+  const { t } = useTranslation();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="cursor-pointer opacity-0 group-hover:opacity-100 data-[state=open]:opacity-100 transition-all delay-150">
@@ -15,14 +18,14 @@ export const ActionsMenu = ({ onDelete, onEdit }: { onDelete: () => void; onEdit
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuItem>
-          <ShareIcon /> Share
+          <ShareIcon /> {t("action-menu.share")}
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={onEdit}>
-          <Edit2 /> Rename
+          <Edit2 /> {t("action-menu.edit")}
         </DropdownMenuItem>
         <DropdownMenuItem onClick={onDelete}>
-          <Trash2 /> Delete
+          <Trash2 /> {t("action-menu.delete")}
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
