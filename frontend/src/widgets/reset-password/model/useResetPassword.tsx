0e23b97a8e7c73beed5b9aa02784156420b89874
Rummiya <rumibashirova@gmail.com>
2025-07-26 17:43:52 +0600

    refactor(reset-password): extract logic from ResetPassword, move to widgets/reset-password

---
+++ frontend/src/widgets/reset-password/model/useResetPassword.tsx
@@ -0,0 +1,33 @@
+import { CodeVerifyForm } from "@/features/auth/ui/forms/CodeVerifyForm";
+import { ConfirmPasswordForm } from "@/features/auth/ui/forms/ConfirmPasswordForm";
+import { EmailEntryForm } from "@/features/auth/ui/forms/EmailEntryForm";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { getTexts } from "./getTexts";
+
+type Step = 0 | 1 | 2;
+
+export const useResetPassword = () => {
+  const navigate = useNavigate();
+  const [step, setStep] = useState<Step>(0);
+  const { titles, descriptions } = getTexts();
+
+  const renderStep = () => {
+    switch (step) {
+      case 0:
+        return <EmailEntryForm onSuccess={() => setStep(1)} />;
+      case 1:
+        return <CodeVerifyForm onSuccess={() => setStep(2)} />;
+      case 2:
+        return <ConfirmPasswordForm onSuccess={() => navigate("/")} />;
+      default:
+        return null;
+    }
+  };
+
+  return {
+    title: titles[step],
+    description: descriptions[step],
+    renderStep,
+  };
+};
